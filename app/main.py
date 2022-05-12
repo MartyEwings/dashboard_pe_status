cc93237d6e1b7a27b5525ae3f93034c6071e0a05
Bartosz Blizniak <blizniak98@hotmail.co.uk>
2022-05-12 23:41:46 +0100

    prototype

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-import json, os
-from readline import parse_and_bind
+import json, os, logging
+from queue import Empty
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -15,28 +16,21 @@ def home():
 # Failing nodes
 @app.route("/failing")
 def failing():
-    data = parse()
-    failed = data["nodes"]["failing"]
-    filter_nodes = data["nodes"]["details"]
-    new_dic = {}
-    for filter_node in filter_nodes:
-        if filter_node in failed:
-            new_dic[filter_node] = filter_nodes[filter_node]
-    return render_template('failing.html', failing=new_dic)
+    failing_nodes = failing_info()
+    return render_template('failing.html', failing=failing_nodes)
 
-# @app.route("/passing")
-# def failing():
-#     data = parse()
-#     passing = data["nodes"]["passing"]
-    
-#     return render_template('passing.html', passing=passing)
+@app.route("/failed_node_info/<string:node>", methods=['GET'])
+def failed_node_info(node):
+    data = failing_info(node)
+    app.logger.error(data)
+    return render_template('failed_node_info.html', node=node, failing_nodes_info=data)
 
 @app.route("/all")
 def all():
-    data = parse()
-    failing = data["nodes"]["failing"]
-    passing = data["nodes"]["passing"]
-    return render_template('all.html', failing_nodes=failing, passing_nodes=passing)
+    failed_info = failing_info()
+    passed_info = passing_info()
+    total = len(failed_info) + len(passed_info)
+    return render_template('all.html', server_total=total, failing_nodes_info=failed_info, passing_nodes_info=passed_info, total_pass=len(passed_info), total_fail=len(failed_info), total_pass_percentage=round(len(passed_info)/total*100, 2), total_fail_percentage=round(len(failed_info)/total*100, 2))
 
 def parse():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
@@ -45,12 +39,28 @@ def parse():
     data = json.load(json_file)
     return data
 
-# def failing_info(fqdn):
-#     data = parse()
-#     failed_info = []
-#     if data["details"] == fqdn:
-#         failed_info.append(data["details"]["failed_tests_detials"])
-#     return failed_info
+def failing_info(node=""):
+    data = parse()
+    failed = data["nodes"]["failing"]
+    filter_nodes = data["nodes"]["details"]
+    return_info = {}
+    if len(node) != 0:
+        return_info = filter_nodes[node]
+    else:
+        for filter_node in failed:
+            if filter_node in failed:
+                return_info[filter_node] = filter_nodes[filter_node]
+    return return_info
+
+def passing_info():
+    data = parse()
+    passing = data["nodes"]["passing"]
+    filter_nodes = data["nodes"]["details"]
+    nodes_with_passing = {}
+    for filter_node in filter_nodes:
+        if filter_node in passing:
+            nodes_with_passing[filter_node] = filter_nodes[filter_node]
+    return nodes_with_passing   
 
 if __name__ == "__main__":
     app.run(debug=True)
